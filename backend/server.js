4877c7a2d31e0f0764c2057ca9df5db7682a6b84
Cathy Yu <z5590011@ad.unsw.edu.au>
2025-12-27 22:18:09 +0800

    connected frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "Backend is running" })
+})
+
+app.listen(3001, () => {
+  console.log("Backend running on http://localhost:3001");
+});
