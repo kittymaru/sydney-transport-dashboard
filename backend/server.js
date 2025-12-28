c4a0b5c3fbcc95ca1e9c0ee9f4d1c6ec7cab13db
Cathy Yu <z5590011@ad.unsw.edu.au>
2025-12-28 23:33:10 +0800

    added backend routes

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,48 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+// Test route
 app.get("/api/health", (req, res) => {
   res.json({ status: "Backend is running" })
-})
+});
+
+// Search for stops
+app.get("/search", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.transport.nsw.gov.au/v1/tp/stop_finder?outputFormat=rapidJSON&type_sf=stop&name_sf=${req.query}&coordOutputFormat=EPSG:4326`, {
+      headers: {
+        Authorization: `apikey ${process.env.NSW_API_KEY}`,
+        Accept: "application/json"
+      }
+    });
+
+    // filter for type stop before returning
+    const data = await response.text();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error searching for stops" });
+  }
+});
+
+// Get departure information from stop
+app.get("/departures", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.transport.nsw.gov.au/v1/tp/stop_finder?outputFormat=rapidJSON&coordOutputFormat=EPSG:4326&type_dm=stop&name_dm=${Number(req.query)}`, {
+        headers: {
+          Authorization: `apikey ${process.env.NSW_API_KEY}`,
+          Accept: "application/json"
+        }
+      }
+    );
+
+    const data = await response.text();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching departure information" });
+  }
+});
 
 app.listen(3001, () => {
   console.log("Backend running on http://localhost:3001");
