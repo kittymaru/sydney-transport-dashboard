17ad40846e8e9c4f1645668e853a210487fcab72
Cathy Yu <z5590011@ad.unsw.edu.au>
2025-12-31 21:51:33 +0800

    added search result display

--- web-frontend/src/Results.jsx
+++ web-frontend/src/Results.jsx
@@ -6,14 +6,24 @@ function ResultsPage() {
   const params = new URLSearchParams(location.search);
   const searchValue = params.get("search");
 
-  const [results, setResults] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getResults = async () => {
       const searchResults = await fetch(`http://localhost:3001/search?name=${searchValue}`);
       const data = await searchResults.json();
-      console.log(data.locations.filter(result => result.type === "stop" && (result.modes.includes(1) || result.modes.includes(2))));
-      setResults(data.locations.filter(result => result.type === "stop" && (result.modes.includes(1) || result.modes.includes(2))));
+      const filtered = data.locations.filter(
+        result =>
+          result.type === "stop" &&
+          Array.isArray(result.modes) &&
+          (result.modes.includes(2) || result.modes.includes(1)) &&
+          result.properties.mainLocality !== "Victoria"
+      );
+      
+      console.log(filtered);
+      setResults(filtered);
+      setLoading(false);
     }
 
     getResults();
@@ -22,6 +32,19 @@ function ResultsPage() {
   return (
     <>
       <h1 className="font-mono font-bold mb-10">stop results for: {searchValue}</h1>
+
+      {!loading && (
+        results.map((stop) => (
+          <div
+            key={stop.id}
+            className="w-full border-2 border-orange-300 rounded p-4 shadow-sm hover:shadow-md transition bg-white mb-5"
+          >
+            <h2 className="font-semibold text-lg">{stop.disassembledName}</h2>
+
+            <p>{stop.id}</p>
+          </div>
+        ))
+      )}
     </>
   )
 }
